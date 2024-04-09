c5d358a776ff3697c76960ec4a04d74fd8e7e18c
Thendo <thendo@Thendos-MacBook-Pro.local>
2024-04-09 18:10:22 +0200

    Setup and created tables for games and users

---
+++ src/main/java/org/packt/mygamelist/MyGameListApp.java
@@ -0,0 +1,13 @@
+package org.packt.mygamelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameListApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameListApp.class, args);
+	}
+
+}
---
+++ src/test/java/org/packt/mgldatabase/MgLdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.packt.mgldatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgLdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
