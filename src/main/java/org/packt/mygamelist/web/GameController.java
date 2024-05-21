b47a86a40dd17073379afcbec4fc5f5e0020ed83
Thendo20 <thendorabali43@gmail.com>
2024-05-21 08:53:41 +0200

    Swagger implementation

--- src/main/java/org/packt/mygamelist/web/GameController.java
+++ src/main/java/org/packt/mygamelist/web/GameController.java
@@ -1,5 +1,9 @@
 package org.packt.mygamelist.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.packt.mygamelist.domain.Game;
 import org.packt.mygamelist.services.GameService;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +14,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-
+@Tag(name = "Game Controller", description = "Controller for managing games")
 @RestController
 @RequestMapping("api/games")
 public class GameController {
@@ -21,6 +25,10 @@ public class GameController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all available or unavailable games")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of games returned successfully")
+    })
     public ResponseEntity<List<Game>> getGames(@RequestParam(value = "gameAvail", required = false) Boolean gameAvail) {
         if (gameAvail == null) {
             List<Game> games = gameService.findAll();
@@ -32,30 +40,50 @@ public class GameController {
     }
 
     @GetMapping("/game/{name}")
+    @Operation(summary = "Get games by name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Game returned successfully")
+    })
     public ResponseEntity<Optional<Game>> getGameByName(@PathVariable("name") String name) {
         Optional<Game> game = gameService.findByName(name);
         return ResponseEntity.ok().body(game);
     }
 
     @GetMapping("/byPrice/{price}")
+    @Operation(summary = "Get games by price")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "List of game/s returned successfully")
+    })
     public ResponseEntity<List<Game>> getGamesByPrice(@PathVariable("price") int price) {
         List<Game> games = gameService.findByPrice(price);
         return ResponseEntity.ok().body(games);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete game by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Game deleted successfully")
+    })
     public ResponseEntity<Game> deleteGameById(@PathVariable("id") Long id) {
         gameService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/byName/{name}")
+    @Operation(summary = "Delete game by name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Game deleted successfully")
+    })
     public ResponseEntity<Game> deleteGameByName(@PathVariable("name") String name) {
         gameService.deleteByName(name);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{name}")
+    @Operation(summary = "Update existing game")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Game updated successfully")
+    })
     public ResponseEntity<Game> updateGame(@RequestBody Game updatedGame) {
         Optional<Game> updated = gameService.update(updatedGame);
         return updated
@@ -71,6 +99,10 @@ public class GameController {
     }
 
     @PostMapping
+    @Operation(summary = "Create game")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Game created successfully")
+    })
     public ResponseEntity<Game> createGame(@RequestBody Game game) {
         Game created = gameService.create(game);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
